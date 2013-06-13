bba2574d0371127a3df2a8bd46455c839d124bb7
Ned Zimmerman <ned@bight.ca>
2013-06-13 14:17:25 -0300

    Fixed issue #63 (select2 fields should support placeholder attribute via data-placeholder tag).

--- custom_metadata.php
+++ custom_metadata.php
@@ -1034,6 +1034,7 @@ class custom_metadata_manager {
 					break;
 				case 'select' :
 					$select2 = ( $field->select2 ) ? ' class="custom-metadata-select2" ' : ' ';
+					$select2 .= ( $field->placeholder ) ? ' data-placeholder="'. $field->placeholder . '" ' : ' ';
 					printf( '<select id="%s" name="%s"%s>', esc_attr( $field_slug ), esc_attr( $field_id ), $select2 );
 					foreach ( $field->values as $value_slug => $value_label ) {
 						printf( '<option value="%s"%s>', esc_attr( $value_slug ), selected( $v, $value_slug, false ) );
@@ -1073,6 +1074,7 @@ class custom_metadata_manager {
 						break;
 					}
 					$select2 = ( $field->select2 ) ? ' class="custom-metadata-select2" ' : ' ';
+					$select2 .= ( $field->placeholder ) ? ' data-placeholder="'. $field->placeholder . '" ' : ' ';
 					printf( '<select name="%s" id="%s"%s>', esc_attr( $field_id ), esc_attr( $field_slug ), $select2 );
 					foreach ( $terms as $term ) {
 						printf( '<option value="%s"%s>%s</option>', esc_attr( $term->slug ), selected( $v, $term->slug, false ), esc_html( $term->name ) );
@@ -1144,6 +1146,7 @@ class custom_metadata_manager {
 			switch ( $field->field_type ) :
 				case 'multi_select' :
 					$select2 = ( $field->select2 ) ? ' class="custom-metadata-select2" ' : ' ';
+					$select2 .= ( $field->placeholder ) ? ' data-placeholder="'. $field->placeholder . '" ' : '';
 					printf( '<select id="%s" name="%s"%smultiple>', esc_attr( $field_slug ), esc_attr( $field_id ), $select2 );
 					foreach ( $field->values as $value_slug => $value_label ) {
 						printf( '<option value="%s"%s>', esc_attr( $value_slug ), selected( in_array( $value_slug, $value ), true, false ) );
