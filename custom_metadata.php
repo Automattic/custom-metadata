d74ac1f650b6596995a53e9e2271f0a071abd0b3
Ned Zimmerman <ned@bight.ca>
2013-07-26 12:01:40 -0400

    Escaped `$field->placeholder` as per @mjangda.

--- custom_metadata.php
+++ custom_metadata.php
@@ -1219,7 +1219,7 @@ class custom_metadata_manager {
 					break;
 				case 'select' :
 					$select2 = ( $field->select2 ) ? ' class="custom-metadata-select2" ' : ' ';
-					$select2 .= ( $field->placeholder ) ? ' data-placeholder="'. $field->placeholder . '" ' : ' ';
+					$select2 .= ( $field->placeholder ) ? ' data-placeholder="'. esc_attr( $field->placeholder ) . '" ' : ' ';
 					printf( '<select id="%s" name="%s"%s>', esc_attr( $field_slug ), esc_attr( $field_id ), $select2 );
 					foreach ( $field->values as $value_slug => $value_label ) {
 						printf( '<option value="%s"%s>', esc_attr( $value_slug ), selected( $v, $value_slug, false ) );
@@ -1259,7 +1259,7 @@ class custom_metadata_manager {
 						break;
 					}
 					$select2 = ( $field->select2 ) ? ' class="custom-metadata-select2" ' : ' ';
-					$select2 .= ( $field->placeholder ) ? ' data-placeholder="'. $field->placeholder . '" ' : ' ';
+					$select2 .= ( $field->placeholder ) ? ' data-placeholder="'. esc_attr( $field->placeholder ) . '" ' : ' ';
 					printf( '<select name="%s" id="%s"%s>', esc_attr( $field_id ), esc_attr( $field_slug ), $select2 );
 					foreach ( $terms as $term ) {
 						printf( '<option value="%s"%s>%s</option>', esc_attr( $term->slug ), selected( $v, $term->slug, false ), esc_html( $term->name ) );
@@ -1300,7 +1300,7 @@ class custom_metadata_manager {
 			switch ( $field->field_type ) :
 				case 'multi_select' :
 					$select2 = ( $field->select2 ) ? ' class="custom-metadata-select2" ' : ' ';
-					$select2 .= ( $field->placeholder ) ? ' data-placeholder="'. $field->placeholder . '" ' : ' ';
+					$select2 .= ( $field->placeholder ) ? ' data-placeholder="'. esc_attr( $field->placeholder ) . '" ' : ' ';
 					printf( '<select id="%s" name="%s"%smultiple>', esc_attr( $field_slug ), esc_attr( $field_id ), $select2 );
 					foreach ( $field->values as $value_slug => $value_label ) {
 						printf( '<option value="%s"%s>', esc_attr( $value_slug ), selected( in_array( $value_slug, $value ), true, false ) );
@@ -1329,7 +1329,7 @@ class custom_metadata_manager {
 						break;
 					}
 					$select2 = ( $field->select2 ) ? ' class="custom-metadata-select2" ' : ' ';
-					$select2 .= ( $field->placeholder ) ? ' data-placeholder="'. $field->placeholder . '" ' : ' ';
+					$select2 .= ( $field->placeholder ) ? ' data-placeholder="'. esc_attr( $field->placeholder ) . '" ' : ' ';
 					printf( '<select name="%s" id="%s"%smultiple>', esc_attr( $field_id ), esc_attr( $field_slug ), $select2 );
 					foreach ( $terms as $term ) {
 						printf( '<option value="%s"%s>%s</option>', esc_attr( $term->slug ), selected( in_array( $term->slug, $value ), true, false ), esc_html( $term->name ) );
