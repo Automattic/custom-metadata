835f04cd003d41e86ef2bd3b7b5bc8b9feada874
Joey Kudish <joachim.kudish@gmail.com>
2013-08-04 15:37:43 -0700

    add some basic tests for metadata groups

--- tests/test-custom-metadata-manager.php
+++ tests/test-custom-metadata-manager.php
@@ -1,6 +1,11 @@
 <?php
 class Test_Custom_Metadata_Manager extends WP_UnitTestCase {
 
+	public $group_slug = 'test_group';
+	public $group_label = 'Test Group';
+	public $non_existant_group_slug = 'this_group_does_not_exist';
+	public $post_metadata = array();
+
 	function setUp() {
 		parent::setUp();
 		$this->load_plugin();
@@ -15,6 +20,24 @@ class Test_Custom_Metadata_Manager extends WP_UnitTestCase {
 		add_action( 'custom_metadata_manager_init', array( $this, '_add_a_group' ) );
 		do_action( 'admin_init' );
 		$this->post_metadata = &custom_metadata_manager::instance()->metadata['post'];
+	}
+
+	function _add_a_group(){
+		x_add_metadata_group( $this->group_slug, array( 'post' ), array(
+			'label' => $this->group_label,
+		) );
+	}
+
+	function test_post_has_added_group() {
+		$this->assertArrayHasKey( $this->group_slug, custom_metadata_manager::instance()->metadata['post'] );
+	}
+
+	function test_post_has_not_added_nonexistant_group() {
+		$this->assertArrayNotHasKey( $this->non_existant_group_slug, custom_metadata_manager::instance()->metadata['post'] );
+	}
+
+	function test_group_label_is_correct() {
+		$this->assertEquals( $this->group_label, $this->post_metadata[$this->group_slug]->label );
 	}
 
 }
